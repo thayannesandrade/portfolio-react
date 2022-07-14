de1bdca804467f8815676ddc0d7d3355d9f762fb
Thayanne Andrade <thayannesandrade@gmail.com>
2022-07-14 11:20:05 -0300

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css"
+
+
+function Header() {
+    return (
+        <>
+            <div className="header_title">
+                <h1>Meu portfólio reprograma</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header
